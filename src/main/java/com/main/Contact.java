36f7a1773c0472b9a1fe137c455be1d399e1fcb4
Boris Mitioglov <boris.mitioglov@NetCracker.com>
2014-08-25 13:21:44 +0400

    Initial

---
+++ src/main/java/com/main/Contact.java
@@ -0,0 +1,31 @@
+package com.main;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CONTACTS")
+public class Contact {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "FIRSTNAME")
+    private String firstname;
+
+    @Column(name = "LASTNAME")
+    private String lastname;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "TELEPHONE")
+    private String telephone;
+
+    // Getters and setters
+}
